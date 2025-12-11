fe3208f3e3318cbb2038172cf40ad82eb944373e
HongHong <qizhang@hainanu.edu.cn>
2025-12-11 16:13:01 +0800

    增加知识图谱类型

---
+++ 思维导图2/data.js
@@ -0,0 +1,82 @@
+const graphData = {
+    "name": "Knowledge Graph",
+    "children": [
+        {
+            "name": "Computer Science",
+            "children": [
+                {
+                    "name": "Algorithms",
+                    "children": [
+                        { "name": "Sorting" },
+                        { "name": "Searching" },
+                        { "name": "Graph Theory" }
+                    ]
+                },
+                {
+                    "name": "Data Structures",
+                    "children": [
+                        { "name": "Arrays" },
+                        { "name": "Linked Lists" },
+                        { "name": "Trees" }
+                    ]
+                },
+                {
+                    "name": "AI",
+                    "children": [
+                        { "name": "Machine Learning" },
+                        { "name": "Neural Networks" },
+                        { "name": "NLP" }
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "Mathematics",
+            "children": [
+                {
+                    "name": "Algebra",
+                    "children": [
+                        { "name": "Linear Algebra" },
+                        { "name": "Abstract Algebra" }
+                    ]
+                },
+                {
+                    "name": "Calculus",
+                    "children": [
+                        { "name": "Derivatives" },
+                        { "name": "Integrals" }
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "Physics",
+            "children": [
+                {
+                    "name": "Mechanics",
+                    "children": [
+                        { "name": "Newton's Laws" },
+                        { "name": "Kinematics" }
+                    ]
+                },
+                {
+                    "name": "Thermodynamics"
+                }
+            ]
+        },
+        {
+            "name": "Chemistry",
+            "children": [
+                { "name": "Organic" },
+                { "name": "Inorganic" }
+            ]
+        },
+        {
+            "name": "Biology",
+            "children": [
+                { "name": "Genetics" },
+                { "name": "Ecology" }
+            ]
+        }
+    ]
+};
